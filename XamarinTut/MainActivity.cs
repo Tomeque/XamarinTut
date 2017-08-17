8e1d9cf7e82b7c195e6063f1aae4c75630ca5dae
tfedorowicz <tfedorowicz@3TCTK32>
2017-08-17 14:57:27 +0200

    22:22

--- XamarinTut/MainActivity.cs
+++ XamarinTut/MainActivity.cs
@@ -1,20 +1,99 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using XamarinTut.Resources.Model;
+using System.Collections.Generic;
+using XamarinTut.Resources;
 
 namespace XamarinTut
 {
     [Activity(Label = "XamarinTut", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        TextView txtNumber;
+        int number;
+
+        ListView lstData;
+        List<Person> lstSource = new List<Person>();
+        DBOperations db;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
 
-            // git testing purposes ### 2
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+
+            //Create DataBase
+            db = new DBOperations();
+            db.CreateDB();
+
+            lstData = FindViewById<ListView>(Resource.Id.listView);
+
+            var edtName = FindViewById<EditText>(Resource.Id.edtName);
+            var edtAge = FindViewById<EditText>(Resource.Id.edtAge);
+            var edtEmail = FindViewById<EditText>(Resource.Id.edtEmail);
+
+            var btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            var btnEdit = FindViewById<Button>(Resource.Id.btnEdit);
+            var btnDelete = FindViewById<Button>(Resource.Id.btnDelete);
+
+            //Loaddata
+            LoadData();
+
+            //Event
+            btnAdd.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.InsertIntoTablePerson(person);
+                LoadData();
+            };
+
+            btnEdit.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.updateTablePerson(person);
+                LoadData();
+            };
+
+            btnDelete.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+                db.deleteTablePerson(person);
+                LoadData();
+            };
+        }
+
+        private void LoadData()
+        {
+            lstSource = db.selectTablePerson();
+            var adapter = new ListViewAdapter(this, lstSource);
+            lstData.Adapter = adapter;
         }
     }
 }
---
+++ XamarinTut/Resources/DataHelper/DBOperations.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System.IO;
+using SQLite;
+using Android.Util;
+using XamarinTut.Resources.Model;
+
+namespace XamarinTut
+{
+    class DBOperations
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        public bool CreateDB()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.CreateTable<Person>();
+                    //connection.GetMapping<Person>();
+                    return true;
+                }
+            }
+            catch(SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool InsertIntoTablePerson(Person person)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.Insert(person);
+                    return true;
+                }
+            }
+            catch(SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public List<Person> selectTablePerson()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    return connection.Table<Person>().ToList();
+                    //connection.GetMapping<Person>();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+
+        public bool updateTablePerson(Person person)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.Query<Person>("UPDATE Person set Name=?,Age=?,Email=? WHERE Id=?", person.Name, person.Age, person.Email, person.Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool deleteTablePerson(Person person)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.Delete(person);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool selectQueryTablePerson(int Id)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.Query<Person>("SELECT * FROM Person WHERE Id=?", Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+    }
+}
+
